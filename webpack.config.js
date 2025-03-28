c0d8f8b6013a6e52b9b627803a0b99d9f4a8fe8b
Vansh5632 <vanshgilhotra8885@gmail.com>
2025-03-28 11:41:50 +0530

    lazy-compilation setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,11 @@
 import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default {
-  mode: 'development', // Switch to 'production' for tree-shaking demo
-  entry: [
-    'webpack-hot-middleware/client', // Enable HMR
-    './src/client/index.js'
-  ],
+  mode: 'development',
+  entry: './src/client/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
@@ -18,19 +14,25 @@ export default {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-env'] }
+        }
+      },
+      {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'] // Process CSS for HMR
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true // Enables CSS Modules
+            }
+          }
+        ]
       }
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ],
-  optimization: {
-    usedExports: true, // Enable tree-shaking by marking unused exports
-    sideEffects: true // Remove side-effect-free modules if unused
-  },
-  devtool: 'source-map' // For debugging
+  }
 };
